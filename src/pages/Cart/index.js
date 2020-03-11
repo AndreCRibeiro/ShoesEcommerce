e4b81eb916503b7cbda5cd013ff184485a95375d
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2020-03-11 18:12:31 -0300

    feat/paginaHome

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Cart() {
-    return <h1>Carrinho</h1>;
+    return (
+        <Container>
+            <table />
+        </Container>
+    );
 }
