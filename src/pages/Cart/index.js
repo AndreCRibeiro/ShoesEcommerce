a04c959d195efba024de24ca86ac7237f02fa6b7
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2020-03-24 15:07:14 -0300

    feat/refatoraçãoRedux

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,9 +6,11 @@ import {
 } from 'react-icons/md';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
     return (
         <Container>
             <ProductTable>
@@ -58,12 +60,7 @@ function Cart({ cart, dispatch }) {
                             <td>
                                 <button
                                     type="button"
-                                    onClick={() =>
-                                        dispatch({
-                                            type: 'REMOVE_FROM_CART',
-                                            id: product.id,
-                                        })
-                                    }
+                                    onClick={() => removeFromCart(product.id)}
                                 >
                                     <MdDelete size={20} color="#7159c1" />
                                 </button>
@@ -87,6 +84,9 @@ function Cart({ cart, dispatch }) {
 
 const mapStateToProps = state => ({
     cart: state.cart,
-})
+});
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
